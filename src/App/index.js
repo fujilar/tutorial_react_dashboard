d32ee1540388904becaa218859b40caf343cc41d
Stephen Lee <lyz_mj@hotmail.com>
2024-11-14 23:39:27 +0800

    commit and add welcome component

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,47 @@
+import React, { Component } from 'react';
+import WelcomeMessage from './WelcomeMessage';
 import './App.css';
 
-function App() {
-  return (
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div`
+  color: green;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`;
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+// const Button = styled.button`
+//   background-color: ${props => props.primary ? 'blue' : 'gray'};
+//   color: white;
+//   padding: 10px 15px;
+//   border: none;
+//   border-radius: 5px;
+//   cursor: pointer;
+
+//   &:hover {
+//     background-color: ${props => props.primary ? 'darkblue' : 'darkgray'};
+//   }
+// `;
+
+class App extends Component {
+  render() {
+    return (
     <div>
-      Hello World
+      <WelcomeMessage/>
+      <MyButton>Click me!</MyButton>
+      <MyButton primary>Click me!</MyButton>
+      <TomatoButton>Click me!</TomatoButton>
+      {/* <Button primary>Primary</Button>
+      <Button>Secondary</Button> */}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
